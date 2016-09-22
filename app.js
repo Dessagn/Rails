abf8468ee733fc7a8c9d93fa077345d51443ceba
Dessagn <temesgen.dessalegn@gmail.com>
2016-09-22 13:44:51 +0300

    dir? created?

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 (function(){
+	'use strict';
 
-	
+	var app = angular.module('myFirstModule', []);
+	app.controller('MyFirstController', ['$scope', function($scope){
+		$scope.name = '';
+		var count = 100;
+		$scope.characterCounter = function (name){
+			if($scope.name===''){ 
+				return '__';
+			}else{
+				return $scope.name.length;
+			}
+		};
+	}]);
 
 })();
